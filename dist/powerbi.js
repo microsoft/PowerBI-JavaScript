262c2260f222fd07ae0c8f763ad4fa7874d851f7
Wallace Breza <wabreza@microsoft.com>
2016-07-08 13:40:52 -0700

    Regenerated 'dist' folder

--- dist/powerbi.js
+++ dist/powerbi.js
@@ -1,4 +1,4 @@
-/*! powerbi-client v1.1.0 | (c) 2016 Microsoft Corporation MIT */
+/*! powerbi-client v1.1.1 | (c) 2016 Microsoft Corporation MIT */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
