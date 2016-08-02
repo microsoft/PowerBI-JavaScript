24f782d7d35a9eac4407287e7ad08b84f4f8b19c
Matt Mazzola <mattm@microsoft.com>
2016-08-02 15:48:23 -0700

    Fix exports in module entry point to only expose required types. Add the new types IFilterable, Page, and Visual

--- src/powerbi.ts
+++ src/powerbi.ts
@@ -1,15 +1,30 @@
 import * as service from './service';
 import * as factories from './factories';
 import * as models from 'powerbi-models';
+import { IFilterable } from './ifilterable';
 
 export {
+    IFilterable,
     service,
     factories,
     models
 };
-export * from './report';
-export * from './tile';
-export * from './embed';
+export {
+    Report
+} from './report';
+export {
+    Tile
+} from './tile';
+export {
+    IEmbedConfiguration,
+    Embed
+} from './embed';
+export {
+    Page
+} from './page';
+export {
+    Visual
+} from './visual';
 
 declare global {
     interface Window {
