cba25559d686680ca5c0dfb1951314a8b9f0e180
Matt Mazzola <mattm@microsoft.com>
2016-08-26 13:27:43 -0700

    Update August 26, 2016 1:27 PM

---
+++ demo/app/defaults.js
@@ -0,0 +1,50 @@
+$(function () {
+  var models = window['powerbi-client'].models;
+
+  console.log('Scenario 5: Default Page and/or Filter');
+
+  var staticReportUrl = 'https://powerbiembedapi.azurewebsites.net/api/dxt/reports/c4d31ef0-7b34-4d80-9bcb-5974d1405572';
+  var $defaultPageReportContainer = $('#reportdefaults');
+  var defaultPageReport;
+  var defaultPageName = 'ReportSection2';
+  var defaultFilter = new models.AdvancedFilter({
+    table: "Store",
+    column: "Name"
+  }, "Or", [
+      {
+        operator: "Contains",
+        value: "Wash"
+      },
+      {
+        operator: "Contains",
+        value: "Park"
+      }
+    ]);
+
+  // Init
+  fetch(staticReportUrl)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json()
+          .then(function (embedConfig) {
+            var defaultsEmbedConfig = $.extend({}, embedConfig, {
+              pageName: defaultPageName,
+              filter: defaultFilter.toJSON(),
+              settings: {
+                filterPaneEnabled: true,
+                navContentPaneEnabled: true
+              }
+            });
+
+            defaultPageReport = powerbi.embed($defaultPageReportContainer.get(0), defaultsEmbedConfig);
+            return defaultPageReport;
+          });
+      }
+      else {
+        return response.json()
+          .then(error => {
+            throw new Error(error);
+          });
+      }
+    });
+});
