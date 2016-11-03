3d4a86b0d18f51f59905d443e75e2e17c01ab6bf
ali-hamud <alihamud@microsoft.com>
2016-11-03 08:39:22 +0200

    update dist folder, update powerbi-models version and increase package version

--- dist/config.d.ts
+++ dist/config.d.ts
@@ -1,4 +1,4 @@
-/*! powerbi-client v2.1.1 | (c) 2016 Microsoft Corporation MIT */
+/*! powerbi-client v2.2.0 | (c) 2016 Microsoft Corporation MIT */
 declare const config: {
     version: string;
     type: string;
