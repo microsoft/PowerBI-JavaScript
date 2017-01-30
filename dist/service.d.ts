c489dc56820964a868d7edad1279a79c285a3ab5
ali-hamud <alihamud@microsoft.com>
2017-01-30 10:53:57 +0200

    increment version to create new package.

--- dist/service.d.ts
+++ dist/service.d.ts
@@ -1,4 +1,4 @@
-/*! powerbi-client v2.2.5 | (c) 2016 Microsoft Corporation MIT */
+/*! powerbi-client v2.2.6 | (c) 2016 Microsoft Corporation MIT */
 import * as embed from './embed';
 import { Report } from './report';
 import { Dashboard } from './dashboard';
