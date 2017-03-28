54f4d4892c7831fc7bc7e018dd4b230abaa389ce
ali-hamud <ali-hamud@users.noreply.github.com>
2017-03-28 12:10:16 +0300

    refresh and reload do not return a value.

--- demo/code-demo/scripts/codesamples.js
+++ demo/code-demo/scripts/codesamples.js
@@ -496,7 +496,7 @@ function _Report_Reload() {
     // Reload the displayed report
     report.reload()
         .then(function (result) {
-            Log.log(result);
+            Log.logText("Reloaded.");
         })
         .catch(function (errors) {
             Log.log(errors);
@@ -513,7 +513,7 @@ function _Report_Refresh() {
     // Refresh the displayed report
     report.refresh()
         .then(function (result) {
-            Log.log(result);
+            Log.logText("Refreshed.");
         })
         .catch(function (errors) {
             Log.log(errors);
