0cd8acffa476e19437f59f166338497c1e1f052c
Ali Hamud <alihamud@microsoft.com>
2017-05-11 12:20:34 +0000

    Merged PR 20565: Call refresh to get new token
    Call refresh to get new token from Live sample backend.
    algorithm:
    when receive a token with expiration, set a timer until the expiration (- one minute), and call live sample backend api to get new token.

--- demo/v2-demo/scripts/step_authorize.js
+++ demo/v2-demo/scripts/step_authorize.js
@@ -2,18 +2,42 @@ var reportUrl = 'http://powerbiembedapiv2.azurewebsites.net/api/Reports/SampleRe
 var datasetUrl = 'http://powerbiembedapiv2.azurewebsites.net/api/Reports/SampleCreate';
 var dashboardUrl = 'http://powerbiembedapiv2.azurewebsites.net/api/Dashboards/SampleDashboard';
 
+var LastReportSampleUrl = null;
+var ReportRefreshTokenTimer = 0;
+var DashboardRefreshTokenTimer = 0;
+
 const EntityType = {
     Report : "Report",
     Dashboard : "Dashboard",
 };
 
-function FetchUrlIntoSession(url) {
+function FetchUrlIntoSession(url, updateCurrentToken) {
     return fetch(url).then(function (response) {
         if (response.ok) {
             return response.json()
             .then(function (embedConfig) {
                 setSession(embedConfig.embedToken.token, embedConfig.embedUrl, embedConfig.id);
                 SetSession(SessionKeys.SampleId, embedConfig.id);
+
+                if (updateCurrentToken)
+                {
+                    var embedContainerId = (embedConfig.type === "report") ? "embedContainer" : "dashboardContainer";
+                    var embedContainer = powerbi.embeds.find(function(embedElement) {return (embedElement.element.id == embedContainerId)});
+                    if (embedContainer)
+                    {
+                        embedContainer.setAccessToken(embedConfig.embedToken.token);
+                    }
+                }
+
+                if (embedConfig.type === "report")
+                {
+                    LastReportSampleUrl = url;
+                    TokenExpirationRefreshListener(embedConfig.minutesToExpiration, EntityType.Report);
+                }
+                else
+                {
+                    TokenExpirationRefreshListener(embedConfig.minutesToExpiration, EntityType.Dashboard);
+                }
             });
         }
         else {
@@ -25,19 +49,53 @@ function FetchUrlIntoSession(url) {
     });
 }
 
+function TokenExpirationRefreshListener(minutesToExpiration, entityType) {
+    var updateAfterMilliSeconds = (minutesToExpiration - 2) * 60 * 1000;
+
+    if (entityType == EntityType.Report)
+    {
+        if (ReportRefreshTokenTimer)
+        {
+            console.log("step current Report Embed Token update threads.");
+            clearTimeout(ReportRefreshTokenTimer);
+        }
+
+        console.log("Report Embed Token will be updated in " + updateAfterMilliSeconds + " milliseconds.");
+        ReportRefreshTokenTimer = setTimeout(function() {
+            if (LastReportSampleUrl)
+            {
+                FetchUrlIntoSession(LastReportSampleUrl, true /* updateCurrentToken */);
+            }
+        }, updateAfterMilliSeconds);
+    }
+    else if (entityType == EntityType.Dashboard)
+    {
+        if (DashboardRefreshTokenTimer)
+        {
+            console.log("step current Dashboard Embed Token update threads.");
+            clearTimeout(DashboardRefreshTokenTimer);
+        }
+
+        console.log("Dashboard Embed Token will be updated in " + updateAfterMilliSeconds + " milliseconds.");
+        DashboardRefreshTokenTimer = setTimeout(function() {
+            FetchUrlIntoSession(dashboardUrl, true /* updateCurrentToken */);
+        }, updateAfterMilliSeconds);
+    }
+}
+
 function LoadSampleReportIntoSession() {
     SetSession(SessionKeys.EntityType, EntityType.Report);
-    return FetchUrlIntoSession(reportUrl);
+    return FetchUrlIntoSession(reportUrl, false /* updateCurrentToken */);
 }
 
 function LoadSampleDatasetIntoSession() {
     SetSession(SessionKeys.EntityType, EntityType.Report);
-    return FetchUrlIntoSession(datasetUrl);
+    return FetchUrlIntoSession(datasetUrl, false /* updateCurrentToken */);
 }
 
 function LoadSampleDashboardIntoSession() {
     SetSession(SessionKeys.EntityType, EntityType.Dashboard);
-    return FetchUrlIntoSession(dashboardUrl);
+    return FetchUrlIntoSession(dashboardUrl, false /* updateCurrentToken */);
 }
 
 function OpenEmbedStepWithSample(entityType) {
