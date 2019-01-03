8f2c781c7cd33a5aacefeab8ffc81cc234bff887
Ali Hamud <alihamud@microsoft.com>
2019-01-03 14:25:47 +0000

    Merged PR 15942: update versions of powerbi-models and poweri-javascript
    update versions.

--- dist/powerbi-client.d.ts
+++ dist/powerbi-client.d.ts
@@ -1,4 +1,4 @@
-/*! powerbi-client v2.6.6 | (c) 2016 Microsoft Corporation MIT */
+/*! powerbi-client v2.6.7 | (c) 2016 Microsoft Corporation MIT */
 declare module "util" {
     /**
      * Raises a custom event with event data on the specified HTML element.
