906174ee00a8aa420ced99ca6649d76d08bcd560
Ali Hamud <alihamud@microsoft.com>
2019-10-15 07:53:11 +0000

    Merged PR 51174: new package version - 2.10.2
    new package version - 2.10.2

--- dist/powerbi.js
+++ dist/powerbi.js
@@ -1,4 +1,4 @@
-/*! powerbi-client v2.10.1 | (c) 2016 Microsoft Corporation MIT */
+/*! powerbi-client v2.10.2 | (c) 2016 Microsoft Corporation MIT */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -1195,7 +1195,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ (function(module, exports) {
 
 	var config = {
-	    version: '2.10.1',
+	    version: '2.10.2',
 	    type: 'js'
 	};
 	Object.defineProperty(exports, "__esModule", { value: true });
